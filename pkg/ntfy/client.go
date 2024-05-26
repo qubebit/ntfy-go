e6c8285652204c472fcd00cac47f9cfbce23c5ce
Elliot <git@lazercube.com>
2024-05-26 19:56:05 +0100

    headers + validator

--- pkg/ntfy/client.go
+++ pkg/ntfy/client.go
@@ -8,29 +8,36 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type (
 	Client struct {
 		httpClient *http.Client
+		validator  *validator.Validate
 		host       *url.URL
+		headers    http.Header
 	}
 
 	PublishOpts struct {
-		Message *Message
+		Message *Message    `validate:"required"`
+		Headers http.Header `validate:"-"`
 	}
 
 	PublishResult struct {
-		ID      string `json:"id"`      // :"bUhbhgmmbeW0"
-		Time    int    `json:"time"`    // :1685150791
-		Expires int    `json:"expires"` // :1685193991
-		Event   string `json:"event"`   // :"message"
-		Topic   string `json:"topic"`   // :"Server"
-		Message string `json:"message"` // :"triggered"
+		ID      string `json:"id"`
+		Time    int    `json:"time"`
+		Expires int    `json:"expires"`
+		Event   string `json:"event"`
+		Topic   string `json:"topic"`
+		Message string `json:"message"`
 	}
 
 	Options struct {
 		HTTPClient *http.Client
+		Validator  *validator.Validate
+		Headers    http.Header
 		Host       string
 	}
 
@@ -38,13 +45,26 @@ type (
 )
 
 var (
-	ErrNoServer = errors.New("server is nil")
-	ErrNoTopic  = errors.New("topic is nil")
+	ErrMissingHost       = errors.New("missing host")
+	ErrMissingHTTPClient = errors.New("missing http client")
+	ErrMissingValidator  = errors.New("missing validator")
 )
 
 func WithHTTPClient(client *http.Client) Option {
 	return func(o *Options) {
 		o.HTTPClient = client
+	}
+}
+
+func WithValidator(v *validator.Validate) Option {
+	return func(o *Options) {
+		o.Validator = v
+	}
+}
+
+func WithHeaders(headers http.Header) Option {
+	return func(o *Options) {
+		o.Headers = headers
 	}
 }
 
@@ -58,11 +78,24 @@ func WithHost(host string) Option {
 func New(opts ...Option) (*Client, error) {
 	options := &Options{
 		HTTPClient: http.DefaultClient,
+		Validator:  validator.New(),
 		Host:       "https://ntfy.sh",
+		Headers:    http.Header{"Content-Type": []string{"application/json"}},
 	}
-
 	for _, o := range opts {
 		o(options)
+	}
+
+	if options.HTTPClient == nil {
+		return nil, ErrMissingHTTPClient
+	}
+
+	if options.Validator == nil {
+		return nil, ErrMissingValidator
+	}
+
+	if options.Host == "" {
+		return nil, ErrMissingHost
 	}
 
 	host, err := url.Parse(options.Host)
@@ -72,22 +105,43 @@ func New(opts ...Option) (*Client, error) {
 
 	return &Client{
 		httpClient: options.HTTPClient,
+		validator:  options.Validator,
+		headers:    options.Headers,
 		host:       host,
 	}, nil
 }
 
-func (t *Client) Publish(ctx context.Context, opts *PublishOpts) (*PublishResult, error) {
+func (c *Client) Publish(ctx context.Context, opts *PublishOpts) (*PublishResult, error) {
+	if err := c.validator.Struct(opts); err != nil {
+		return nil, err
+	}
+
 	buf, err := json.Marshal(opts.Message)
 	if err != nil {
 		return nil, err
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, t.host.String(), bytes.NewReader(buf))
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, c.host.String(), bytes.NewReader(buf))
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := t.httpClient.Do(req)
+	for key, values := range c.headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	if opts.Headers != nil {
+		for key, values := range opts.Headers {
+			req.Header.Del(key)
+			for _, value := range values {
+				req.Header.Add(key, value)
+			}
+		}
+	}
+
+	resp, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
